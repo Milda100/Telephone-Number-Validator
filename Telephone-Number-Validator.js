387cc627eb87fb124b4f1c4d61c0a9755846cbdd
Jellyfish <157160973+Milda100@users.noreply.github.com>
2025-03-13 07:57:49 -0400

    Update Telephone-Number-Validator.js

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -4,3 +4,12 @@ const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
 
+const isCorrectInput = (input) => {
+
+    if (input === "") {
+        alert("Please provide a phone number");
+          return;
+    }
+}
+
+checkBtn.addEventListener("click", isCorrectInput);
