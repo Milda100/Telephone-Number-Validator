d1a85f49e8aeed5e7840db81101a6e869a139a94
Jellyfish <157160973+Milda100@users.noreply.github.com>
2025-03-14 05:30:13 -0400

    Update Telephone-Number-Validator.js

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -4,25 +4,32 @@ const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
 
-const isCorrectInput = (input) => {
+const checkInput = (userInput) => {
 
-    if (input === "") {
+    if (userInput.value === "") {
         alert("Please provide a phone number");
           return;
     }
 
-    const countryCodeRegex = /^1\s?/;
-    const areaCodeRegex = /[0-9]{3}|\([0-9]{3}\)/;
-    const symbolRegex = /-|\s/;
-    const phoneNoRegex = /[0-9]{3}[-|\s]?[0-9]{4}$/;
+    const countryCodeRegex = "^1\\s?";
+    const areaCodeRegex = "[0-9]{3}|\\([0-9]{3}\\)";
+    const symbolRegex = "-|\\s";
+    const phoneNoRegex = "[0-9]{3}[-|\\s]?[0-9]{4}$";
 
-    const isPhoneNo = new RegExp(`${countryCodeRegex}${areaCodeRegex}${symbolRegex}${phoneNoRegex}`);
+    const fullPhoneNoRegex = new RegExp(`${countryCodeRegex}${areaCodeRegex}${symbolRegex}${phoneNoRegex}`);
 
+    if (fullPhoneNoRegex === userInput){
+        results.innerHTML = `<p>Valid US number: ${fullPhoneNoRegex}</p>`;
+    } else {
+        results.innerHTML = `<p>Invalid US number: ${userInput}</p>`
+    }
+
+    
 }
 
 // const clearResults = () => {
 //     results.innerHTML = "";
 // }
 
-checkBtn.addEventListener("click", isCorrectInput);
+checkBtn.addEventListener("click", checkInput(userInput.value));
 clearBtn.addEventListener("click", clearResults);
