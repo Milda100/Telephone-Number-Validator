309769e0f4a3ff423b0d58e6dd32921fe7665000
Jellyfish <157160973+Milda100@users.noreply.github.com>
2025-03-14 04:26:32 -0400

    Update Telephone-Number-Validator.js

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -3,18 +3,12 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
-// const regex =   /^1\s    [0-9]{3}         -  [0-9]{3}-[0-9]{4}$
-//                 |^1\s    \([0-9]{3}\)    \s  [0-9]{3}-[0-9]{4}$
-//                 |^1      \([0-9]{3}\)    \s  [0-9]{3}-[0-9]{4}$
-//                 |^1\s    [0-9]{3}        \s  [0-9]{3}\s[0-9]{4}$
-//                 |^                           [0-9]{10}$
-//                 |^       [0-9]{3}        -   [0-9]{3}-[0-9]{4}$
-//                 |^       \([0-9]{3}\)        [0-9]{3}-[0-9]{4}$/
-
 const countryCodeRegex = /^1\s?/;
 const areaCodeRegex = /[0-9]{3}|\([0-9]{3}\)/;
 const symbolRegex = /-|\s/;
-const phoneNoRegex = 
+const phoneNoRegex = /[0-9]{3}[-|\s]?[0-9]{4}$/;
+
+const isPhoneNo = new RegExp(`${countryCodeRegex}${areaCodeRegex}${symbolRegex}${phoneNoRegex}`);
 
 
 
